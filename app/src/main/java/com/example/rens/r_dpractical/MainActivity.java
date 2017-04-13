88acba48dafa53849dbd6d43dac796dd99489885
RensAlthuis <Rens.althuis@gmail.com>
2017-04-13 11:32:40 +0200

    re-initial commit

---
+++ app/src/main/java/com/example/rens/r_dpractical/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rens.r_dpractical;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
