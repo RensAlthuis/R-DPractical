fca44e415715dd8bd17b73055bd0685358c636cd
RensAlthuis <rens.althuis@gmail.com>
2017-04-22 20:02:20 +0200

    Simpel menu toegevoegd

--- app/src/main/java/com/example/rens/r_dpractical/MainActivity.java
+++ app/src/main/java/com/example/rens/r_dpractical/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.rens.r_dpractical;
 
-import android.graphics.drawable.Drawable;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
 
-        Board board = new Board(this);
+        Button start = (Button) findViewById(R.id.StartButton);
+       final Activity activity = this;
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_game);
+                Board board = new Board(activity);
+            }
+        });
     }
 }
